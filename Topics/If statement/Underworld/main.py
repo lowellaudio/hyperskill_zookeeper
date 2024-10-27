01a393fbd5857284d99a2c08333a22bfbec4a872
oleksii.shlapak <oleksii.shlapak@oleksiishlapaks-MacBook-Pro14.local>
2024-10-27 13:15:48 +0200

    First commit

---
+++ Topics/If statement/Underworld/main.py
@@ -0,0 +1,9 @@
+# read the value from the input
+coin = bool(int(input()))  # don't modify this line, please
+
+if coin == 1:
+    print("Welcome to Charon's boat!")
+else:
+    print()
+
+print("There is no turning back.")
---
+++ Topics/While loop/Factorial/main.py
@@ -0,0 +1,8 @@
+
+n = int(input())
+fact_calc = n
+
+while 1 < n < 100:
+    n -= 1
+    fact_calc *= n
+print(fact_calc)
